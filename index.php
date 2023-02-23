b426066289a081c034340e9d7509bed331423529
aSquare <artur.anissimov@gmail.com>
2023-02-23 13:43:44 +0200

    Objects Protected/Public Variable Accessors(get) Static constructors

--- index.php
+++ index.php
@@ -1,19 +1,56 @@
 <?php
 
-class Invoice {
+class Team {
 
-}
+    protected $name;
+    protected $members = [];
 
-class InvoiceItem {
+    public function __construct($name, $members = []) {
+        $this->name = $name;
+        $this->members = $members;
+    }
 
-}
+    public static function start(... $params) {
+        return new static(... $params);
+    }
 
-class AchievementBadge {
-    public $title;
-    public $description;
-    public $points;
+    public function name() {
+        return $this->name;
+    }
 
-    public function awardTo($user) {
-        // Award user some badge
+    public function members() {
+        return $this->members;
+    }
+
+    public function add($name) {
+        $this->members[] = $name;
+    }
+
+    public function cancel() {
+
+    }
+
+    public function manager() {
+
     }
 }
+
+class Member {
+    protected $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function lastViewed() {
+
+    }
+}
+
+$acme = Team::start('Acme', [
+    new Member('John Doe'),
+    new Member('Jane Doe'),
+]);
+
+var_dump($acme->members());
+
