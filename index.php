04c243b68b1f7734a8b68a80c8fb0e4a9d70f43a
aSquare <artur.anissimov@gmail.com>
2023-02-23 20:04:14 +0200

    Interfaces The interface is like a contract between an employee and a boss ;)

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <?php
 
-abstract class AchievementType {
-    public function name() {
-        $class = (new ReflectionClass($this))->getShortName();
-
-        return trim(preg_replace('/[A-Z]/', ' $0', $class));
-    }
-
-    public function icon() {
-        return strtolower(str_replace(' ', '-', $this->name())) . '.png';
-    }
-
-    abstract public function qualifier($user);
-}
-class FirstThousandPoints extends AchievementType {
-
-    public function qualifier($user) {
-
-    }
+interface Newsletter {
+    public function subscribe($email);
 }
 
-class ReachTop50 extends AchievementType {
+class CampaignMonitor implements Newsletter {
 
-    public function qualifier($user)
+    public function subscribe($email)
     {
-        // TODO: Implement qualifier() method.
+        die('Subscribing with Campaign Monitor');
     }
-
 }
 
-$achievement = new ReachTop50();
+class Drip implements Newsletter {
 
-echo $achievement->qualifier('user');
+    public function subscribe($email)
+    {
+        die('Subscribing with Drip');
+    }
+}
+
+
+class NewsLetterSubscriptionsController {
+
+    public function store(Newsletter $newsletter) {
+        $email = 'joe@exampl.com';
+        $newsletter->subscribe($email);
+    }
+}
+
+$controller = new NewsLetterSubscriptionsController();
+$controller->store(new Drip());
+
